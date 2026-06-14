92b1fc7f2f9bdc7835c113afff0af23ef75e61cd
Богдан <bohmanin@gmail.com>
2026-06-15 02:46:22 +0300

    Revert "First commit"
    This reverts commit abd5a74033919e28469cfaa9e7d6e0005af9d5a7.

--- src/main/java/com/recipebook/dto/CategoryDTO.java
+++
@@ -1,7 +0,0 @@
-package com.recipebook.dto;
-
-public record CategoryDTO(
-        Long id,
-        String name,
-        String description
-) {}
--- src/main/java/com/recipebook/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recipebook.repository;
-
-import com.recipebook.entity.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByCategoryId(Long categoryId);
-}
